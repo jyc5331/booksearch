d4f3d8bce9c72e508e0c6a377e7c13824fd5fa32
jyc5331 <jcosgrovecoding@gmail.com>
2021-04-16 10:47:37 -0400

    typeDefs adjusted for challenge

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,25 +5,18 @@ const typeDefs = gql`
     _id: ID
     username: String
     email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
+    savedBooks: [Book]
+    bookCount: Int
   }
 
-  type Thought {
+  type Book {
     _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
   }
 
   type Auth {
